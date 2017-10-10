834eae653c478b925ed37395a57427205152e467
Eugene Sia <eugene@eugenesia.co.uk>
2017-10-10 23:01:32 +0100

    Create Forecast component

--- App.js
+++ App.js
@@ -13,6 +13,9 @@ import {
   View
 } from 'react-native';
 
+import Forecast from './Forecast';
+
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
@@ -25,7 +28,13 @@ export default class App extends Component<{}> {
   constructor() {
     super();
     this.state = {
-      zip: ''
+      zip: '',
+      // Mock data for weather forecast.
+      forecast: {
+        main: 'Clouds',
+        description: 'few clouds',
+        temp: 45.7
+      }
     };
     this._handleTextChange = this._handleTextChange.bind(this);
   }
@@ -42,16 +51,15 @@ export default class App extends Component<{}> {
         <Text style={styles.welcome}>
           You input {this.state.zip}.
         </Text>
+        {/* Weather forecast */}
+        <Forecast
+          main={this.state.forecast.main}
+          description={this.state.forecast.description}
+          temp={this.state.forecast.temp} />
         {/* Enter zip code. */}
         <TextInput
           style={styles.input}
           onSubmitEditing={this._handleTextChange} />
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
       </View>
     );
   }
@@ -69,11 +77,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
   // TextInput to enter the zip code.
   input: {
     fontSize: 20,
@@ -81,3 +84,4 @@ const styles = StyleSheet.create({
     height: 40
   }
 });
+
